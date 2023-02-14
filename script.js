aa12efffdbac35ca8c5738595f554dcc733ce92a
Varun Sudrik <varunsudrik99@gmail.com>
2023-02-14 06:38:50 +0530

    commit

---
+++ script.js
@@ -0,0 +1,5 @@
+window.addEventListener("load",()=>{
+    let form = document.querySelector("#new-task-form")
+    let input = document.querySelector("#new-task-input")
+    let list = document.querySelector("#tasks")
+})
