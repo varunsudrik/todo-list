8ecf42bec7137b5fd99ee85374dbbc7cab4f7f42
Varun Sudrik <varunsudrik99@gmail.com>
2023-02-15 14:49:48 +0530

    commit ui

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 window.addEventListener("load",()=>{
-    let form = document.querySelector("#new-task-form")
-    let input = document.querySelector("#new-task-input")
-    let list = document.querySelector("#tasks")
+    let form = document.querySelector("#new-task-form");
+    let inputt = document.querySelector("#new-task-input");
+    let list = document.querySelector("#tasks");
+
+    let dodo = inputt.value
+
+    console.log(dodo)
 })
