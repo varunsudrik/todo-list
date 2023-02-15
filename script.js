23d085fb3ae43d597623a015d4a5d264e47884b4
Varun Sudrik <varunsudrik99@gmail.com>
2023-02-15 23:45:14 +0530

    final

--- script.js
+++ script.js
@@ -17,11 +17,56 @@ window.addEventListener('load',()=>{
     
         let task_content_el = document.createElement("div")
         task_content_el.classList.add("content")
-        task_content_el.innerText = task
     
         task_el.appendChild(task_content_el)
-        console.log(task_el)
+
+        let task_input_el = document.createElement("input")
+        task_input_el.classList.add("text")
+        task_input_el.nodeType="text"
+        task_input_el.value = task
+        task_input_el.setAttribute("readonly", "readonly")
+
+        task_content_el.appendChild(task_input_el)
+
+        let task_action_el = document.createElement("div")
+        task_action_el.classList.add("actions")
+
+        let task_edit_el = document.createElement("button")
+        task_edit_el.classList.add("edit")
+        task_edit_el.innerHTML = "Edit"
+        task_action_el.appendChild(task_edit_el)
+
+
+        let task_del_el = document.createElement("button")
+        task_del_el.classList.add("delete")
+        task_del_el.innerHTML = "Delete"
+        task_action_el.appendChild(task_del_el)
+
+        task_el.appendChild(task_action_el)
+
         list_el.appendChild(task_el)
+        input.value= ''
+
+        task_edit_el.addEventListener("click",()=>{
+            if (task_edit_el.innerText.toLowerCase() == "edit"){
+            task_input_el.removeAttribute("readonly")
+            task_input_el.focus
+            task_edit_el.innerText = "Save"
+        } else{ 
+            task_input_el.setAttribute("readonly","readonly")
+            task_edit_el.innerText = "Edit"
+
+            
+
+        }
+
+        })
+
+        task_del_el.addEventListener("click",()=>{
+            list_el.removeChild(task_el)
+            
+        })
+
     
 
 
