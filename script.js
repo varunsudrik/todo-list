4aa304ab62e4c013b89e86432cbcc9b9533f153f
Varun Sudrik <varunsudrik99@gmail.com>
2023-02-15 17:40:14 +0530

    commit
    added new task functionality

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.addEventListener('load',()=>{
     const form = document.querySelector("#new-task-form")
     const input = document.querySelector("#new-task-input")
-    const list_ele = document.querySelector(".task")
+    const list_el = document.querySelector("#tasks")
 
     form.addEventListener("submit", (e)=>{
         e.preventDefault();
@@ -12,8 +12,23 @@ window.addEventListener('load',()=>{
             return
         }
 
+        let task_el = document.createElement("div")
+        task_el.classList.add("task")
+    
+        let task_content_el = document.createElement("div")
+        task_content_el.classList.add("content")
+        task_content_el.innerText = task
+    
+        task_el.appendChild(task_content_el)
+        console.log(task_el)
+        list_el.appendChild(task_el)
+    
+
 
 
     })
+
+
+
 })
 
